fa48cb2c0912442356037fabf5a11d58b2241291
pooja-1810 <gurralapoojasree1998@gmail.com>
2025-01-20 13:41:49 -0800

    created 05-variables file

--- 05-variables.sh
+++ 05-variables.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
 
 echo "Please enter your username"
-
-read USERNAME
-
+read -s USERNAME
 echo "Username entered is :$USERNAME"
+
+echo "Please enter your password"
+read -s PASSWORD
+echo "password entered is :$PASSWORD"
+"
