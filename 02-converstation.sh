6f5c5ac400717a7559c26a03d011832c3d276c26
pooja-1810 <gurralapoojasree1998@gmail.com>
2025-01-20 12:29:08 -0800

    i created converstation file

---
+++ 02-converstation.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+echo "Ramesh : hi, how are you?"
+echo "Suresh : i am good. how are you doing?"
+echo "Ramesh : im doing good , whats going on?"
+echo "Suresh: i started learning shell scripting."
