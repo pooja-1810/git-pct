3539f1cbefb1122a073702fc87ea8d9d2d989c54
pooja-1810 <gurralapoojasree1998@gmail.com>
2025-01-20 13:00:22 -0800

    variables

---
+++ 03-variables.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+PERSON1=Ramesh
+PERSON2=Suresh
+
+echo "$PERSON1 : hi, how are you?"
+echo "$PERSON2 : i am good. how are you doing?"
+echo "$PERSON1 : im doing good , whats going on?"
+echo "$PERSON2: i started learning shell scripting."
