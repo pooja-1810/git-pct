5da1d6b4b1b37a19a5b0ccaad8c45297d1d2b7c3
pooja-1810 <gurralapoojasree1998@gmail.com>
2025-01-20 13:18:07 -0800

    created 04-variables file

---
+++ 04-variables.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+PERSON1=$1
+PERSON2=$2
+
+echo "$PERSON1 : hi, how are you?"
+echo "$PERSON2 : i am good. how are you doing?"
+echo "$PERSON1 : im doing good , whats going on?"
+echo "$PERSON2: i started learning shell scripting."
