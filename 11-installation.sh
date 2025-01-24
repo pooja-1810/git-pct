a5c9091856209abe86328e46645b450f4627f74f
pooja-1810 <gurralapoojasree1998@gmail.com>
2025-01-23 21:31:43 -0800

    conditions

--- 11-installation.sh
+++ 11-installation.sh
@@ -3,15 +3,10 @@
 USERID=$(id -u)
 #echo "user id is $USERID"
 
-#if [ $USERID -ne 0 ]
-#then
-#   echo "Please run this script with root privileges"
-#fi 
+if [ $USERID -ne 0 ]
+then
+    echo "Please run this script with root privileges"
+    exit 1
+fi 
 
-#dnf install git -y
-
-ls -ltr
-
-lss -ltr
-
-ls -l
+dnf install git -y
