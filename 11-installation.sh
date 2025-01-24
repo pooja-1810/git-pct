145736fa4d80bdb28bbb8f135d27dceac36dd785
pooja-1810 <gurralapoojasree1998@gmail.com>
2025-01-23 21:42:59 -0800

    conditions

--- 11-installation.sh
+++ 11-installation.sh
@@ -9,4 +9,14 @@ then
     exit 1
 fi 
 
-dnf install git -y
+#dnf install git -y
+
+dnf list installed git 
+
+if [ $? -ne 0 ]
+then
+    echo "git is not installed, going to install it"
+    dnf install git -y
+else
+    echo "git is already installed,"
+fi
