6e4e2f3101f9ae6790a613a8f6a8ce5efa9dfe81
pooja-1810 <gurralapoojasree1998@gmail.com>
2025-02-04 22:38:21 -0800

    shell

--- 18-delete-old-logs.sh
+++ 18-delete-old-logs.sh
@@ -2,10 +2,18 @@
 
 SOURCE_DIR=/c/devops/daws-81s/logs
 
-If [ -d $SOURCE_DIR ]
+if [ -d $SOURCE_DIR ]
 then 
     echo "$SOURCE_DIR Exists"
 else
     echo "$SOURCE_DIR does not exists"
     exit 1
 fi
+
+FILES=$(find $SOURCE_DIR -name "*.log" -mtime +14)
+echo "Files : $FILES"
+
+while IFS= read -r line #IFS,internal field separator, empty it   will ignore white spaces. -r is for not to ignore special charaters like /
+do
+    echo "Deleting line: $line"
+done <<< $FILES
